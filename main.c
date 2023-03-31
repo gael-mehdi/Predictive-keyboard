05b95ae3996a23499fdf99dde210d2bf190616a5
jeremyL23 <jeremylouis2312@gmail.com>
2023-03-31 10:28:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
-#include <stdio.h>
+#include <ncurses.h>
 
 int main() {
-    printf("Entrez des mots : ");
-
-    char c[100];
-	int i=0;
-    while ( (c[i]=getchar())!= '\n') {
-		
-
-    }
-
-    printf("\n");
+    // Initialiser ncurses
+    initscr();
+    // Créer une fenêtre de 10 lignes et 20 colonnes, en commençant aux coordonnées (2, 2)
+    WINDOW* fenetre = newwin(10, 20, 2, 2);
+    // Activer les couleurs
+    start_color();
+    // Définir une paire de couleurs
+    init_pair(1, COLOR_RED, COLOR_WHITE);s
+    // Appliquer la paire de couleurs à la fenêtre
+    wbkgd(fenetre, COLOR_PAIR(1));
+    // Rafraîchir l'écran pour afficher la fenêtre
+    refresh();
+    // Attendre une entrée utilisateur
+    getch();
+    // Fermer la fenêtre et quitter ncurses
+    delwin(fenetre);
+    endwin();
     return 0;
 }
+
