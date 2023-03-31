8030f972b1bed0644c639bc9da48f393b9de1cd8
gael-mehdi <gm.lelay@outlook.com>
2023-03-31 11:37:52 +0200

    Update main.c
    on peut mtn supprimer du texte

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
  #include <ncurses.h>
 
  #define KEY_ESC 27
+ #define KEY_SUPPR 127
 
 int main()
 {
@@ -11,12 +12,17 @@ int main()
     noecho(); // Ne pas afficher les caractères saisis
     keypad(stdscr, TRUE); // Active les touches spéciales (F1, flèches, etc.)
 
-    printw("Entrez du texte. Appuyez sur ESC pour quitter.");
+    printw("Entrez du texte. Appuyez sur ESC pour quitter.\n");
 
     do {
         ch = getch();
-        if (ch != (char)KEY_ESC) {
+        if (ch != (char)KEY_ESC && ch != (char)KEY_SUPPR ) {
             printw("%c", ch); // Affiche le caractère saisi
+        }
+        if (ch == (char)KEY_SUPPR){
+            move(getcury(stdscr), getcurx(stdscr) - 1); // Se déplace d'un caractère à gauche
+            delch(); // Supprime le caractère précédent le curseur
+            refresh(); // Rafraîchit l'écran
         }
     } while (ch != (char)KEY_ESC);
 
