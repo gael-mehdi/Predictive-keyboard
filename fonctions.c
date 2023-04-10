5e3adc25bdf11e253e4f2ec4246b06239dc7fbda
gael-mehdi <gm.lelay@outlook.com>
2023-04-10 20:32:03 +0200

    Ajout table de hashage

--- fonctions.c
+++ fonctions.c
@@ -1,10 +1,37 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <ncurses.h>
 #include <string.h>
+#include <stdbool.h>
+#include <math.h>
 
 #define KEY_ESC 27
 #define KEY_SUPPR 127
+#define MAX_WORD_LENGHT	40		/* Maximum word length */
+#define dictionaryFileName "mots_courants.txt"
+
+typedef struct _element
+
+{
+
+	char			word[MAX_WORD_LENGHT];
+
+	struct _element* next;
+
+} Element;
+
+
+
+typedef struct _hashTable
+{
+
+	unsigned int	size;
+	unsigned int	nbOccupiedEntries;
+	unsigned int	nbElements;
+	Element**		Elements;
+
+} HashTable;
 
 // Cette fonction prend trois entiers en entrée et retourne le minimum de ces trois valeurs
 int min(int x, int y, int z) {
@@ -47,7 +74,7 @@ int levenshtein_distance(char *s1, char *s2) {
 // Cette fonction permet d'ouvrir une fenêtre avec la bibliothèque ncurses et de taper du texte dedans
 void fenetre(){
     char ch;
-    char mot[50]; // les mots feront moins de 50 caracteres
+    char mot[MAX_WORD_LENGHT];
 
     initscr(); // Initialise ncurses
     cbreak(); // Désactive le buffering de ligne
@@ -71,4 +98,75 @@ void fenetre(){
     } while (ch != (char)KEY_ESC);
 
     endwin(); // Ferme ncurses
+}
+
+void initializeHashTable(HashTable* hashTab)
+{
+	hashTab->size = HASH_SIZE;
+	hashTab->nbOccupiedEntries = 0;
+	hashTab->nbElements = 0;
+	hashTab->Elements = (Element**)malloc(hashTab->size * sizeof(Element*));
+
+	for (unsigned int i = 0; i < hashTab->size; i++)
+		hashTab->Elements[i] = NULL;
+}
+
+
+
+void loadDictionaryFromFile(HashTable* hashTab, const char* dictionaryFileName)
+{
+	FILE* fp;
+	fp = fopen(dictionaryFileName, "r");
+
+	if (fp != NULL)
+	{
+		char word[MAX_WORD_LENGHT];
+		while (fscanf(fp, "%s", word) == 1)
+			insertElementToHashTable(hashTab, word);
+		fclose(fp);
+	}
+	else 
+		printf("File not found.");
+}
+
+unsigned long getHashValue(char* string)
+{
+	unsigned long	hashValue = 0;
+	int				i = 0;
+
+	while ((*string) != '\0')
+	{
+		hashValue += hashValue % HASH_SIZE + ((*string) * (int)pow(BASE, i)) % HASH_SIZE;
+		i++;
+		string++;
+	}
+	return hashValue % HASH_SIZE;
+}
+
+void insertElementToHashTable(HashTable* hashTab, char* word)
+{
+	hashTab->nbElements++;
+	unsigned long	i = getHashValue(word);
+	Element* elem = (Element*)malloc(sizeof(Element));
+	strcpy(elem->word, word);
+
+	if (hashTab->Elements[i] == NULL)
+		hashTab->nbOccupiedEntries++;
+
+	elem->next = hashTab->Elements[i];
+	hashTab->Elements[i] = elem;
+}
+
+bool checkExistenceWordInDictionary(HashTable* hashTab, char* word)
+{
+	unsigned long	hashValue = getHashValue(word);
+	Element* elem = hashTab->Elements[hashValue];
+	bool		found = 0;
+
+	while (found == 0 && elem != NULL)
+	{
+		found = (strcmp(word, elem->word) == 0);
+		elem = elem->next;
+	}
+	return found;
 }
--- fonctions.h
+++ fonctions.h
@@ -6,6 +6,26 @@
 #define KEY_ESC 27
 #define KEY_SUPPR 127
 
+typedef struct _element{
+	char word[MAX_WORD_LENGHT];
+	struct _element* next;
+} Element;
+
+
+
+typedef struct _hashTable{
+	unsigned int size;
+	unsigned int nbOccupiedEntries;
+	unsigned int nbElements;
+	Element** Elements;
+} HashTable;
+
 int min(int x, int y, int z);
 int levenshtein_distance(char *s1, char *s2);
 void fenetre();
+void menu(HashTable* hashTab);
+void loadDictionaryFromFile(HashTable* hashTab, const char* dictionaryFileName);
+void insertElementToHashTable(HashTable* hashTab, char* word);
+void initializeHashTable(HashTable* hashTab);
+bool checkExistenceWordInDictionary(HashTable* hashTab, char* word);
+void printHashTableCharacteristics(HashTable* hashTab);
